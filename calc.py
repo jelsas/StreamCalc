2510f6c79b8342e88ea8a53ddc9b1cfc13362aad
Jonathan Elsas <jelsas@cs.cmu.edu>
2010-12-10 16:26:36 -0500

    removing memory-hogging numpy functions, replacing with python alternatives

--- calc.py
+++ calc.py
@@ -51,7 +51,10 @@ $ jot -r 100 | pc log | pc hist
 [4.14,4.60]: ################################
 
 '''
+from __future__ import division
 import numpy as n
+import math as m
+from itertools import imap
 
 def list_formatter(l):
   return '\n'.join( str(x) for x in l )
@@ -59,7 +62,7 @@ def list_formatter(l):
 def hist_formatter(x, tick_char = '#', max_width = 80):
   '''prints a histogram'''
   (vals, bins) = x
-  s_bins = ['%0.2f' % b for b in bins]
+  s_bins = ['%0.2g' % b for b in bins]
   max_bin_len = max(len(x) for x in s_bins)
   s_bins = [x.rjust(max_bin_len) for x in s_bins]
   max_val = max(vals)
@@ -73,22 +76,66 @@ def hist_formatter(x, tick_char = '#', max_width = 80):
                                 tick_char*vals[-1])
   return s
 
+def s_mean_var(data):
+  '''calculates the mean & variance with minimal intermediate data structures.
+  see http://www.johndcook.com/standard_deviation.html'''
+  m_n = 0
+  m_oldM, m_newM, m_oldS, m_newS = 0.0, 0.0, 0.0, 0.0
+  for x in data:
+    m_n += 1
+    if m_n == 1:
+      m_oldM = m_newM = x
+      m_oldS = 0.0
+    else:
+      m_newM = m_oldM + (x - m_oldM) / m_n
+      m_newS = m_oldS + (x - m_oldM)*(x - m_newM)
+      m_oldM, m_oldS = m_newM, m_newS
+  mean = m_newM if m_n > 0 else 0.0
+  var = m_newS / (m_n - 1) if m_n > 1 else 0.0
+  return (mean, var)
+
+def s_mean(data): return s_mean_var(data)[0]
+def s_var(data): return s_mean_var(data)[1]
+def s_std(data): return m.sqrt(s_var(data))
+
+def s_cumsum(data):
+  sum = 0
+  for x in data:
+    sum += x
+    yield x
+
+def s_cumprod(data):
+  prod = 1.0
+  for x in data:
+    prod *= x
+    yield prod
+
+s_exp = lambda data: imap(m.exp, data)
+s_log = lambda data: imap(m.log, data)
+
+def s_prod(data):
+  prod = 1.0
+  for x in data: prod *= x
+  return prod
+
+def l(func):
+  '''Simple function wrapper to convert the input into a list.  Used for
+  making the numpy functions below behave correctly with a generator input.'''
+  return lambda data: func(list(data))
+
 class Command(object):
   '''An individual command, deals with execution & formatting'''
-  formatter = str
-  function = sum
-  help = ''
 
-  def __init__(self, function = None, formatter = None, help = None):
-    if formatter: self.formatter = formatter
-    if function: self.function = function
-    if help: self.help = help
+  def __init__(self, function = None, formatter = str, help = None):
+    self.formatter = formatter
+    self.function = function
+    self.help = help
 
-  def __call__(self, *args):
-    return self.function(*args)
+  def __call__(self, data):
+    return self.function(data) if self.function else data
 
-  def process(self, *args):
-    return self.formatter(self(*args))
+  def process(self, data):
+    return self.formatter(self(data))
 
 class CommandProcessor(object):
   '''Handles processing commands'''
@@ -96,7 +143,7 @@ class CommandProcessor(object):
     self._commands = {}
 
   def register_command(self, name, command = None, function = None,
-                       formatter = None, help = None):
+                       formatter = str, help = None):
     if command:
       self._commands[name] = command
     else:
@@ -110,36 +157,39 @@ class CommandProcessor(object):
                       sorted(self._commands.items())]
     return '\n'.join(all_commands_help)
 
-  def process(self, command, *args):
+  def process(self, command, data):
     try:
       c =self._commands[command]
     except KeyError, e:
       raise ValueError('Command not found: %s' % command)
-    return c.process(*args)
+    return c.process(data)
 
 c = CommandProcessor()
 c.register_command('sum', function=sum, help='Add a list of numbers')
 c.register_command('add', function=sum, help='see sum')
 c.register_command('max', function=max, help='Max')
 c.register_command('min', function=min, help='Min')
-c.register_command('prod', function=n.prod, help='Multiply a list of numbers')
-c.register_command('hist', function=lambda x: n.histogram(x, new=True),
+c.register_command('prod', function=s_prod,
+                   help='Multiply a list of numbers')
+c.register_command('hist', function=lambda x: n.histogram(list(x), new=True),
                    formatter=hist_formatter, help='Produce a histogram')
-c.register_command('mean', function=n.mean, help='Mean')
-c.register_command('median', function=n.median, help='Median')
-c.register_command('var', function=n.var, help='Variance')
-c.register_command('std', function=n.std, help='Standard Deviation')
-c.register_command('cumsum', function=n.cumsum, formatter=list_formatter,
+c.register_command('mean', function=s_mean, help='Mean')
+c.register_command('median', function=l(n.median), help='Median')
+c.register_command('var', function=s_var, help='Variance')
+c.register_command('std', function=s_std, help='Standard Deviation')
+c.register_command('cumsum', function=l(n.cumsum), formatter=list_formatter,
                    help='Cumulative sum')
-c.register_command('cumprod', function=n.cumprod, formatter=list_formatter,
+c.register_command('cumprod', function=s_cumprod, formatter=list_formatter,
                    help='Cumulative product')
-c.register_command('exp', function=n.exp, formatter=list_formatter,
+c.register_command('exp', function=s_exp, formatter=list_formatter,
                    help='Exponentiate every element in the list')
-c.register_command('log', function=n.log, formatter=list_formatter,
+c.register_command('log', function=s_log, formatter=list_formatter,
                    help='Take the log of every element in the list')
-c.register_command('print', function=lambda x: x, formatter=list_formatter,
-                  help='Just print the (cleaned) input')
+c.register_command('print', function=None, formatter=list_formatter,
+                   help='Just print the (cleaned) input')
 c.register_command('help', function=None, help="Print this message")
+c.register_command('rstat', function=s_mean_var,
+                   help='Computes mean & variance with lower memory usage')
 
 
 if __name__ == "__main__":
@@ -159,11 +209,9 @@ Available Commands:
   command = sys.argv[1]
 
   if command == 'help': help_quit(0)
-  if not c.valid_command(command): help_quit(1)
 
-
-  l = [float(x.strip()) for x in fileinput.input(sys.argv[2:]) \
-          if len(x.strip()) > 0 and x[0] != '#']
+  l = (float(x.strip()) for x in fileinput.input(sys.argv[2:]) \
+          if len(x.strip()) > 0 and x[0] != '#')
   try:
     print c.process(command, l)
   except ValueError, e:
